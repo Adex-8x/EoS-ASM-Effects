0e8bda273495a868c3b185a2d8ccb2d0a907afe2
Adex-8x <91418697+Adex-8x@users.noreply.github.com>
2022-01-30 13:31:13 -0500

    Add files via upload
    Checked for stat boosts and modifiers.

--- moves/gen5/psyshock.asm
+++ moves/gen5/psyshock.asm
@@ -30,16 +30,24 @@
 
 		ldr r0,[r4,#+0xb4]
 		ldrb r1,[r0,#+0x1d]
-		push {r0,r1}
+		ldrsh r2,[r0,#+0x2a]
+		ldr r3,[r0,#+0x40]
+		push {r0,r1,r2,r3}
 		ldrb r1,[r0,#+0x1c]
 		strb r1,[r0,#+0x1d]
+		ldrsh r1,[r0,#+0x28]
+		strh r1,[r0,#+0x2a]
+		ldr r1,[r0,#+0x3c]
+		str r1,[r0,#+0x40]
 		mov r0,r9
 		mov r1,r4
 		mov r2,r8
 		mov r3,#0x100
 		bl DealDamage
-		pop {r0,r1}
+		pop {r0,r1,r2,r3}
 		strb r1,[r0,#+0x1d]
+		strh r2,[r0,#+0x2a]
+		str r3,[r0,#+0x40]
 		b MoveJumpAddress
 		.pool
 	.endarea
