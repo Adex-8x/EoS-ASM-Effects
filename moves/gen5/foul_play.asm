92e3ba19ea7d9bbfdd6db3041a84f422529006f2
Adex-8x <91418697+Adex-8x@users.noreply.github.com>
2022-01-30 18:29:25 -0500

    Minor fix with stat multiplier.

--- moves/gen5/foul_play.asm
+++ moves/gen5/foul_play.asm
@@ -36,7 +36,7 @@
 		ldrb r2, [r0, #+0x1a]
 		ldrsh r3, [r0, #+0x24]
 		ldr r12, [r0, #+0x34]
-		push {r2,r3}
+		push {r2,r3,r12}
 		ldrb r2, [r1, #+0x1a]
 		strb r2, [r0, #+0x1a]
 		ldrsh r2, [r1, #+0x24]
@@ -48,9 +48,8 @@
 		mov r2,r8
 		mov r3,#0x100
 		bl DealDamage
-		pop {r2,r3}
+		pop {r2,r3,r12}
 		ldr r0, [r9, #+0xb4]
-		ldr r1, [r4, #+0xb4]
 		strb r2, [r0, #+0x1a]
 		strh r3, [r0, #+0x24]
 		str r12, [r0, #+0x34]
