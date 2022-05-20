3c9f58a81d32c8bc734e1c6fa07398f29a3dd21e
Adex-8x <91418697+Adex-8x@users.noreply.github.com>
2022-05-20 17:37:23 -0400

    there's a function that does it for me lol

--- special_processes/party/get_current_party_size.asm
+++ special_processes/party/get_current_party_size.asm
@@ -1,8 +1,8 @@
 ; ------------------------------------------------------------------------------
-; Get Party Size
-; Checks how many members are in the specified party (0 is Maingame, 1 is Special Episode, 2 is Rescue, and -1 is the current team)!
-; Param 1: team_id
-; Returns: The number of party members (also 0, if unsuccessful).
+; Get Current Party Size
+; Checks how many members are in the current party!
+; No Params
+; Returns: The size of the party!
 ; ------------------------------------------------------------------------------
 
 .relativeinclude on
@@ -17,42 +17,21 @@
 .include "lib/stdlib_us.asm"
 .definelabel ProcStartAddress, 0x022E7248
 .definelabel ProcJumpAddress, 0x022E7AC0
-.definelabel AssemblyPointer, 0x020B0A48
+.definelabel GetPartyMembers, 0x2056C20
 
 ; For EU
 ;.include "lib/stdlib_eu.asm"
 ;.definelabel ProcStartAddress, 0x022E7B88
 ;.definelabel ProcJumpAddress, 0x022E8400
-;.definelabel AssemblyPointer, 0x20B138C
+;.definelabel GetPartyMembers, 0x2056F9C
 
 ; File creation
 .create "./code_out.bin", 0x022E7248 ; For EU: 0x022E7B88
 	.org ProcStartAddress
 	.area MaxSize
 		
-		mov r1,#0
-		cmp r7,#2
-		bgt @@ret
-		ldr r0,=AssemblyPointer
-		ldr r0,[r0]
-		cmp r7,#0
-		addlt r0,r0,#0x9800
-		addlt r0,r0,#0x4c
-		ldrlt r0,[r0]
-		addge r0,r0,#0x9300
-    		addge r0,r0,#0x6C
-		movge r1,#0x1A0
-    		mlage r0,r7,r1,r0
-		mov r1,#0
-@@member_loop:
-		cmp r1,#4
-		bge @@ret
-		ldrb r2,[r0],#+0x68
-		cmp r2,#0
-		addne r1,r1,#1
-		b @@member_loop
-@@ret:
-		mov r0,r1
+		mov r0,#0
+		bl GetPartyMembers
 		b ProcJumpAddress
 		.pool
 	.endarea
